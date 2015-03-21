c710cdcfed88fd535506bfdf969874a693babe73
outadoc <outadoc@gmail.com>
2015-03-21 18:05:06 +0100

    Fix database encoding

--- app/config/services.php
+++ app/config/services.php
@@ -57,7 +57,8 @@
 			'host'     => $config->database->host,
 			'username' => $config->database->username,
 			'password' => $config->database->password,
-			'dbname'   => $config->database->dbname
+			'dbname'   => $config->database->dbname,
+			'charset'  => $config->database->charset
 		));
 	});
 
