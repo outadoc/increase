64c0db3d35eb1612876110f15a29dd9333d205c9
outadoc <outadoc@gmail.com>
2015-03-21 23:09:56 +0100

    Created an action to get a list of usecases for an author and a project

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -3,6 +3,7 @@
 	namespace increase\controllers;
 
 	use increase\models\Projet;
+	use increase\models\Usecase;
 	use Phalcon\Mvc\View;
 
 	class ProjectController extends ControllerBase
@@ -55,6 +56,32 @@
 			$response = new \Phalcon\Http\Response();
 			$response->setHeader("Content-Type", "application/json");
 			$response->setContent(json_encode($devs));
+
+			return $response;
+		}
+
+		public function authorAction($projectId, $authorId)
+		{
+			$usecases = Usecase::find(array(
+				"conditions" => "idDev = ?1 AND idProjet = ?2",
+				"bind"       => array(1 => $authorId, 2 => $projectId)
+			));
+
+			$result = array();
+
+			foreach ($usecases as $usecase) {
+				$result[] = array(
+					"code"     => $usecase->getCode(),
+					"weight"   => $usecase->getPoids(),
+					"nb_tasks" => count($usecase->getTaches())
+				);
+			}
+
+			$this->view->disable();
+
+			$response = new \Phalcon\Http\Response();
+			$response->setHeader("Content-Type", "application/json");
+			$response->setContent(json_encode($result));
 
 			return $response;
 		}
